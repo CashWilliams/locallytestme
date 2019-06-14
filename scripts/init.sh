e888fd33da8cf7baeb47c8911e6eb8726c1559c7
Cash Williams <CashWilliams@gmail.com>
2019-06-14 07:18:39 -0500

    Fixing logging

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,6 +11,3 @@ chmod a+rw /var/log/php
 /bin/sed -i 's/;error_log\ =\ php_errors.log/error_log\ =\ \/var\/log\/php\/php_errors.log/g' /etc/php/7.2/cli/php.ini
 /bin/sed -i 's/;error_log\ =\ php_errors.log/error_log\ =\ \/var\/log\/php\/php_errors.log/g' /etc/php/7.2/apache2/php.ini
 echo "general_log_file = /var/log/mysql/mysql.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-ln -sfT /dev/stderr "/var/log/apache2/error.log";
-ln -sfT /dev/stdout "/var/log/apache2/access.log";
-ln -sfT /dev/stderr "/var/log/php/php_errors.log";
